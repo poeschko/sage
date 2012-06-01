5b35668afb758a678ea3c9a8bbcd626a98812e6a
Jan Pöschko <jan@poeschko.com>
2012-06-01 23:26:23 +0200

    basic class structure

--- devel/sage-lattices/sage/lattices/lattice.py
+++ devel/sage-lattices/sage/lattices/lattice.py
@@ -9,9 +9,9 @@ The class inheritance hierarchy is:
 
 - :class:`Lattice_generic`
 
-  - :class:`LatticeZZ`
+  - :class:`Lattice_ZZ`
 
-    - :class:`Lattice_embedded_in_RR`
+    - :class:`Lattice_ZZ_in_RR`
 
 AUTHORS:
 
@@ -19,39 +19,69 @@ AUTHORS:
 
 """
 
-from sage.modules.free_module import FreeModule_ambient
+from sage.misc.latex import latex
 
-class Lattice_generic(FreeModule_ambient):
-    def __init__(self, base_ring, base):
-        FreeModule_ambient.__init__(self, base_ring, len(base))
-        self.base = base
+from sage.modules.free_module import FreeModule_submodule_with_basis_pid
+from sage.modules.free_module_element import vector
+from sage.matrix.constructor import matrix
+from sage.rings.integer_ring import ZZ
+from sage.rings.real_mpfr import RR
+
+class Lattice_generic(FreeModule_submodule_with_basis_pid):
+    def __init__(self, ambient, gens, coefficient_ring):
+        super(Lattice_generic, self).__init__(ambient, gens, echelonize=False)
+        self._coefficient_ring = coefficient_ring
         
     def _repr_(self):
         """
         Text representation of this lattice.
         """
-        return "Lattice on %s with base %s" % (self.base_ring(), self.base)
+        return "%s-Lattice in %s" % (self._coefficient_ring, self.vector_space())
     
     def _latex_(self):
         """
         LaTeX representation of this lattice.
         """
-        return "\\text{Lattice}"
+        return "\\left\\{\\sum_i a_i v_i\\mid a_i\\in %s,v_i\\in %s\\right\\}" % (
+            latex(self._coefficient_ring), 
+            '\\left\\{%s\\right\\}' % ','.join(latex(gen) for gen in self.gens()))
     
     def _matrix_(self):
         """
         Matrix representation of this lattice.
         """
-        return self.base
+        return matrix(self.gens())
+    
+class Lattice_ZZ(Lattice_generic):
+    def __init__(self, ambient, gens):
+        super(Lattice_ZZ, self).__init__(ambient, gens, ZZ)
         
-def Lattice(base=None, quadratic_form=None):
+class Lattice_ZZ_in_RR(Lattice_ZZ):
+    def __init__(self, gens):
+        dim = len(gens[0])
+        super(Lattice_ZZ_in_RR, self).__init__(RR ** dim, gens)
+   
+''' 
+def lattice_element(value):
+    """
+    A value translated to an element as part of a lattice.
+    Converts lists and tuples to vectors.
+    """
+    if isinstance(value, (list, tuple)):
+        return vector(value)
+    return value
+'''
+        
+def Lattice(vector_space=None, basis=None, coefficient_ring=ZZ, quadratic_form=None):
     """
     The `Lattice` function creates lattices using a given base
     or an underlying quadratic form.
     
     INPUT:
     
-    -  ``base``
+    -  ``vector_space``
+    -  ``basis``
+    -  ``coefficient_ring``
     -  ``quadratic_form``
     
     OUTPUT:
@@ -63,12 +93,21 @@ def Lattice(base=None, quadratic_form=None):
         sage: Lattice([1, 2])
         Lattice on Integer Ring with base [1, 2]
     """
-    
-    if base is not None:
-        if base:
-            return Lattice_generic(base[0].parent(), base)
-        else:
+    if basis is not None:
+        if not basis:
             raise ValueError("base must not be empty")
+        #basis = [lattice_element(element) for element in basis]
+        if vector_space is None:
+            #vector_space = basis[0].parent()
+            basis_matrix = matrix(basis)
+            vector_space = basis_matrix.base_ring() ** basis_matrix.dimensions()[1]
+        if coefficient_ring == ZZ:
+            if vector_space.base_ring() == RR:
+                return Lattice_ZZ_in_RR(basis)
+            else:
+                return Lattice_ZZ(vector_space, basis)
+        else:
+            return Lattice_generic(vector_space, basis, coefficient_ring)
     elif quadratic_form is not None:
         raise NotImplementedError()
     else:
