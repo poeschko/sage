2107fce2baadca305cec9c72bc9e723e70a451a2
Jan Pöschko <jan@poeschko.com>
2012-08-23 00:27:26 +0200

    ** instead of ^

--- devel/sage-lattices/sage/modules/free_quadratic_module.py
+++ devel/sage-lattices/sage/modules/free_quadratic_module.py
@@ -364,11 +364,11 @@ class FreeQuadraticModule_generic(free_module.FreeModule_generic):
             6
         """
         n = self.rank()
-        if n%2 == 0:
-            r = n//2
+        if n % 2 == 0:
+            r = n // 2
         else:
-            r = (n-1)//2
-        return (-1)^r*self.gram_matrix().determinant()
+            r = (n-1) // 2
+        return ((-1) ** r) * self.gram_matrix().determinant()
 
     def gram_matrix(self):
         """
